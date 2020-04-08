75f81085e6bf53c9de10748673ee99ac775a23c3
Alex Cuffia <alex@thecuffiagroup.com>
2020-04-08 15:57:04 -0400

    created cart item comopnent and conencted our cart dropdown with cartitems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
